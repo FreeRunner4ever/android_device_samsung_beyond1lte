ede2ea50f69b3986a8e61b7a5b65e929a4a68e7e
LeDrew2017 <33235594+LeDrew2017@users.noreply.github.com>
2023-06-09 19:49:17 -0400

    beyond1lte: cherish-13 bring-up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beyond1lte.mk
+    $(LOCAL_DIR)/cherish_beyond1lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beyond1lte-eng \
-    lineage_beyond1lte-user \
-    lineage_beyond1lte-userdebug
+    cherish_beyond1lte-eng \
+    cherish_beyond1lte-user \
+    cherish_beyond1lte-userdebug
