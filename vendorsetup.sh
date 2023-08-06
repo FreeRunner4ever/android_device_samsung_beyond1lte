4b6072a53e8013790bff01b6c9c9199960f313cb
jay <jay@pop-os.localdomain>
2023-08-06 19:28:13 -0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,11 +6,11 @@ git clone https://github.com/FreeRunner4ever/android_kernel_samsung_exynos9820 k
 
 # Common tree for exynos9820-common
 rm -rf device/samsung/exynos9820-common
-git clone https://github.com/FreeRunner4ever/android_device_samsung_exynos9820-common device/samsung/exynos9820-common -b elixir
+git clone https://github.com/ProjectElixir-Devices/android_device_samsung_exynos9820-common device/samsung/exynos9820-common
 
 # Device tree for beyond1lte
 rm -rf device/samsung/beyond1lte
-git clone https://github.com/FreeRunner4ever/android_device_samsung_beyond1lte device/samsung/beyond1lte -b elixir
+git clone https://github.com/ProjectElixir-Devices/android_device_samsung_beyond1lte device/samsung/beyond1lte
 
 # Vendor blobs for all exynos9820 devices
 rm -rf vendor/samsung
